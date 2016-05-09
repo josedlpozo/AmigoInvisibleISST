14a0f87bb45f17956c79644a8d417924cb9f89ba
josedlpozo <jmd.pozo@alumnos.upm.es>
2016-05-09 16:17:49 +0200

    add test for userdao

--- test/es/upm/isst/amigoinvisible/datastore/UserDaoImplTest.java
+++ test/es/upm/isst/amigoinvisible/datastore/UserDaoImplTest.java
@@ -29,20 +29,42 @@ public class UserDaoImplTest {
 	@Test
 	public void testSaveUser() {
 		UserDao dao = UserDaoImpl.getInstance();
-		Usuario user = new Usuario("admin", "admin@admin.com", ""+System.currentTimeMillis());
+		Usuario user = new Usuario("admin", "admin@admin.com", String.valueOf(System.currentTimeMillis()));
 		dao.saveUser(user.getUsername(), user.getEmail(), user.getUserId());
 		
+		Usuario user2 = new Usuario("admin", "admin2@admin.com", String.valueOf(System.currentTimeMillis()));
+		dao.saveUser(user2.getUsername(), user2.getEmail(), user2.getUserId());
+		
+		
 		assertEquals("admin", user.getUsername());
+		assertEquals("admin@admin.com", user.getEmail());
 	}
 
 	@Test
 	public void testGetUserByName() {
 		UserDao dao = UserDaoImpl.getInstance();
-		Usuario user = new Usuario("admin", "admin@admin.com", ""+System.currentTimeMillis());
+		
+		Usuario user = new Usuario("admin", "admin@admin.com", String.valueOf(System.currentTimeMillis()));
 		dao.saveUser(user.getUsername(), user.getEmail(), user.getUserId());
+		Usuario user2 = new Usuario("admin2", "admin@admin.com", String.valueOf(System.currentTimeMillis()));
+		dao.saveUser(user2.getUsername(), user2.getEmail(), user2.getUserId());
+		
+		String userId3 = String.valueOf(System.currentTimeMillis());
+		Usuario user3 = new Usuario("admin3", "admin@admin.com", userId3);
+		Usuario user4 = new Usuario("admin4", "admin@admin.com", userId3);
+		dao.saveUser(user3.getUsername(), user3.getEmail(), user3.getUserId());
+		dao.saveUser(user4.getUsername(), user4.getEmail(), user4.getUserId());
 		
 		Usuario result = dao.getUserByName("admin");
+		Usuario result2 = dao.getUserByName("admin2");
+		Usuario result3 = dao.getUserByName("admin3");
+		Usuario result4 = dao.getUserByName("admin4");
+		
 		assertEquals("admin@admin.com", result.getEmail());
+		assertEquals("admin@admin.com", result2.getEmail());
+		assertEquals("admin2", result2.getUsername());
+		assertNull(result3);
+		assertEquals("admin4", result4.getUsername());
 	}
 
 	@Test
@@ -57,35 +79,84 @@ public class UserDaoImplTest {
 	@Test
 	public void testGetUserByEmail() {
 		UserDao dao = UserDaoImpl.getInstance();
-		Usuario user = new Usuario("admin", "admin@admin.com", ""+System.currentTimeMillis());
+		
+		Usuario user = new Usuario("admin", "admin@admin.com", String.valueOf(System.currentTimeMillis()));
 		dao.saveUser(user.getUsername(), user.getEmail(), user.getUserId());
+		Usuario user2 = new Usuario("admin2", "admin2@admin.com", String.valueOf(System.currentTimeMillis()));
+		dao.saveUser(user2.getUsername(), user2.getEmail(), user2.getUserId());
+		
+		String userId3 = String.valueOf(System.currentTimeMillis());
+		Usuario user3 = new Usuario("admin3", "admin3@admin.com", userId3);
+		Usuario user4 = new Usuario("admin4", "admin4@admin.com", userId3);
+		dao.saveUser(user3.getUsername(), user3.getEmail(), user3.getUserId());
+		dao.saveUser(user4.getUsername(), user4.getEmail(), user4.getUserId());
 		
 		Usuario result = dao.getUserByEmail("admin@admin.com");
-		assertEquals("admin", result.getUsername());
+		Usuario result2 = dao.getUserByEmail("admin2@admin.com");
+		Usuario result3 = dao.getUserByEmail("admin3@admin.com");
+		Usuario result4 = dao.getUserByEmail("admin4@admin.com");
+		
+		assertEquals("admin@admin.com", result.getEmail());
+		assertEquals("admin2@admin.com", result2.getEmail());
+		assertEquals("admin2", result2.getUsername());
+		assertNull(result3);
+		assertEquals("admin4", result4.getUsername());
 	}
 
 	@Test
 	public void testUpdateUser() {
 		UserDao dao = UserDaoImpl.getInstance();
-		Usuario user = new Usuario("admin", "admin@admin.com", ""+System.currentTimeMillis());
+	
+		String userId = String.valueOf(System.currentTimeMillis());
+		String userId2 = String.valueOf(System.currentTimeMillis());
+		Usuario user = new Usuario("admin", "admin3@admin.com", userId);
+		Usuario user2 = new Usuario("admin2", "admin4@admin.com", userId2);
 		dao.saveUser(user.getUsername(), user.getEmail(), user.getUserId());
+		dao.saveUser(user2.getUsername(), user2.getEmail(), user2.getUserId());
 		
-		user.setUsername("admin2");
+		user2.setId(userId);
+		dao.updateUser(user2);
+		
+		Usuario result = dao.getUserByName("admin");
+		Usuario result2 = dao.getUserByName("admin2");
+		
+		assertNull(result);
+		assertEquals("admin2", result2.getUsername());
+		
+		user.setId(userId);
 		dao.updateUser(user);
 		
-		Usuario result = dao.getUserByEmail("admin@admin.com");
-		assertEquals("admin2", result.getUsername());
+		Usuario result3 = dao.getUserByName("admin");
+		
+		assertEquals("admin", result3.getUsername());
 	}
 
 	@Test
 	public void testGetUserByID() {
 		UserDao dao = UserDaoImpl.getInstance();
-		String userId = String.valueOf(System.currentTimeMillis());
-		Usuario user = new Usuario("admin", "admin@admin.com", userId);
-		dao.saveUser(user.getUsername(), user.getEmail(), user.getUserId());
 		
-		Usuario result = dao.getUserByID(userId);
-		assertEquals("admin", result.getUsername());
+		String id = String.valueOf(System.currentTimeMillis());
+		Usuario user = new Usuario("admin", "admin@admin.com", id);
+		dao.saveUser(user.getUsername(), user.getEmail(), user.getUserId());
+		String id2 = String.valueOf(System.currentTimeMillis());
+		Usuario user2 = new Usuario("admin2", "admin2@admin.com", id2);
+		dao.saveUser(user2.getUsername(), user2.getEmail(), user2.getUserId());
+		
+		String userId3 = String.valueOf(System.currentTimeMillis());
+		Usuario user3 = new Usuario("admin3", "admin3@admin.com", userId3);
+		Usuario user4 = new Usuario("admin4", "admin4@admin.com", userId3);
+		dao.saveUser(user3.getUsername(), user3.getEmail(), user3.getUserId());
+		dao.saveUser(user4.getUsername(), user4.getEmail(), user4.getUserId());
+		
+		Usuario result = dao.getUserByID(userId3);
+		Usuario result2 = dao.getUserByEmail("admin3@admin.com");
+		Usuario result3 = dao.getUserByID(id);
+		Usuario result4 = dao.getUserByID(id2);
+		
+		assertEquals("admin4", result.getUsername());
+		assertNull(result2);
+		assertEquals("admin@admin.com", result3.getEmail());
+		assertEquals("admin2", result4.getUsername());
 	}
 
 }
